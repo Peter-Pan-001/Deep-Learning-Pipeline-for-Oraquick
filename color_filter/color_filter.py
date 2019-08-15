6a189362f16be1081216da33598fcd5292a455f1
Peter-Pan-001 <31752269+Peter-Pan-001@users.noreply.github.com>
2019-08-15 01:16:00 -0400

    Add files via upload

---
+++ color_filter/color_filter.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+import sys,os
+
+IMG_WIDTH = 96
+IMG_HEIGHT = 64
+IMG_CHANNELS = 1
+
+training_dir = 'crop_training'
+testing_dir = 'crop_testing'
+output_training_dir = 'color_filter_output'
+output_testing_dir = 'color_filter_output'
+
+"""
+lower_red_1 = np.array([0,67,50])
+upper_red_1 = np.array([10,255,255])
+
+lower_red_2 = np.array([170,67,50])
+upper_red_2 = np.array([180,255,255])
+"""
+
+lower_red_1 = np.array([0,65,50])
+upper_red_1 = np.array([10,255,255])
+lower_red_2 = np.array([170,65,50])
+upper_red_2 = np.array([180,255,255])
+
+# main function
+for image_name in os.listdir(training_dir):
+    if "jpg" in image_name or "jpeg" in image_name or "png" in image_name:
+        img = cv2.imread('{}/{}'.format(training_dir, image_name))
+        #img = cv2.resize(img, (600, 140))
+        img = cv2.resize(img, (120, 64))
+        #img = img[:, 50:550]
+        img = img[:, 12:108]
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
+        mask2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
+        mask = mask1 + mask2
+        mask = np.reshape(mask, (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+        cv2.imwrite('./{}/{}'.format(output_training_dir, image_name),mask)
+
+for image_name in os.listdir(testing_dir):
+    if "jpg" in image_name or "jpeg" in image_name or "png" in image_name:
+        img = cv2.imread('{}/{}'.format(testing_dir, image_name))
+        #img = cv2.resize(img, (600, 140))
+        #img = img[:, 50:550]
+        img = cv2.resize(img, (120, 64))
+        img = img[:, 12:108]
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
+        mask2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
+        mask = mask1 + mask2
+        mask = np.reshape(mask, (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+        cv2.imwrite('./{}/{}'.format(output_testing_dir, image_name),mask)
